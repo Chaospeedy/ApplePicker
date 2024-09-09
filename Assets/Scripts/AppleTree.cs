31b8dffd0c23e4b28a847c4de166d83a0ef1e27a
Chaospeedy <seansdilday@gmail.com>
2024-09-08 23:21:43 -0500

    Added AppleTree script
    Movement specifically

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
+    void Start()
+    {
+        //start dropping apples
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //changing direction
+        if(pos.x < -leftAndRightEdge){
+            speed = Mathf.Abs(speed);
+        }else if(pos.x > leftAndRightEdge){
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    void FixedUpdate(){
+
+        if(Random.value < changeDirChance){
+            speed *= -1;
+        }
+    }
+}
