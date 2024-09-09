4d672b3984e75414d984cad9a83bf1f0fe9ea1a1
Chaospeedy <seansdilday@gmail.com>
2024-09-08 23:33:48 -0500

    Added apple dropping script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -13,6 +13,13 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         //start dropping apples
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple(){
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     // Update is called once per frame
